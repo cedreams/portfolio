ebb0541ccee5920fa96b4981f36ca9ad01ae1e2a
Cedric Thonus <me@cedricthonus.be>
2021-09-22 22:00:56 +0200

    test: not the final rendering

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,13 +7,13 @@ export default function Contact() {
     function handleChange(event: any) {
         let dataIn = event.nativeEvent.data;
         let value = event.target.value;
-        
+
         if((dataIn != null 
             && dataIn.match("[0-9]")
             && value.length <= 10) 
             || (dataIn == null 
                 && value.length >= 1)) {
-            setPhone(event.target.value);
+            setPhone(value);
         } 
     }
 
@@ -24,14 +24,25 @@ export default function Contact() {
     return (
         <div id="contact" className="contact-content">
             <div className="contact-title title">Contact</div>
-            <div className="contact-desc"></div>
-            <div className="contact-mail">me@cedricthonus.be</div>
-            <div className="contact-github">https://github.com/cedream</div>
+
+            <div className="contact-item">
+                <img src="assets/images/icons/email.svg"/>
+                me@cedricthonus.be
+            </div>
+            <div className="contact-item">
+                <img src="assets/images/icons/github.svg"/>
+                <a style={{
+                  "textDecoration" : "none",
+                  "color": "white"  
+                }}
+                    href="https://github.com/cedream">https://github.com/cedream</a>
+            </div>
 
             <div className="phone-section">
                 <input type="text" onChange={handleChange} value={phone} className="phone-field" />
                 <span onClick={handleClick} className="phone-submit">Call me</span>
             </div>
+            <div className="phone-desc">Put your number and I'll try to call you back</div>
 
         </div>
     );
