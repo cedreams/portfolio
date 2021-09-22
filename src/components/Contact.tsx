d2f087f4acce96cf82cfdd142c6825bb5fe59af7
Cedric Thonus <me@cedricthonus.be>
2021-09-22 21:32:29 +0200

    feat: add field phone number

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,35 @@
+import React, {useState} from "react";
+
 export default function Contact() {
+
+    const [phone, setPhone] = useState<number>(0);
+
+    function handleChange(event: any) {
+        let dataIn = event.nativeEvent.data;
+        if((dataIn != null 
+            && dataIn.match("[0-9]")
+            && event.target.value.length <= 10) 
+            || (dataIn == null)) {
+            setPhone(event.target.value);
+        } 
+    }
+
+    function handleClick() {
+        //TODO: submit phone number to api.
+    }
+
     return (
         <div id="contact" className="contact-content">
             <div className="contact-title title">Contact</div>
             <div className="contact-desc"></div>
             <div className="contact-mail">me@cedricthonus.be</div>
             <div className="contact-github">https://github.com/cedream</div>
+
+            <div className="phone-section">
+                <input type="text" onChange={handleChange} value={phone} className="phone-field" />
+                <span onClick={handleClick} className="phone-submit">Call me</span>
+            </div>
+
         </div>
     );
 }
