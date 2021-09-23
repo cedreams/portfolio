6f8df4e5972a5b1aff5e353c1fe2d4b27856d17d
Cedric Thonus <me@cedricthonus.be>
2021-09-23 22:23:57 +0200

    refactor: remove phone field from the main branch

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,26 +1,4 @@
-import React, {useState} from "react";
-
 export default function Contact() {
-
-    const [phone, setPhone] = useState<number>(0);
-
-    function handleChange(event: any) {
-        let dataIn = event.nativeEvent.data;
-        let value = event.target.value;
-
-        if((dataIn != null 
-            && dataIn.match("[0-9]")
-            && value.length <= 10) 
-            || (dataIn == null 
-                && value.length >= 1)) {
-            setPhone(value);
-        } 
-    }
-
-    function handleClick() {
-        //TODO: submit phone number to api.
-    }
-
     return (
         <div id="contact" className="contact-content">
             <div className="contact-title title">Contact</div>
@@ -37,13 +15,6 @@ export default function Contact() {
                 }}
                     href="https://github.com/cedream">https://github.com/cedream</a>
             </div>
-
-            <div className="phone-section">
-                <input type="text" onChange={handleChange} value={phone} className="phone-field" />
-                <span onClick={handleClick} className="phone-submit">Call me</span>
-            </div>
-            <div className="phone-desc">Put your number and I'll try to call you back</div>
-
         </div>
     );
 }
