8a3019c71b6297abfaf17e636f47f29acddcbf6e
Cedric Thonus <me@cedricthonus.be>
2021-09-23 22:07:50 +0200

    chore: add page head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import "../styles/global.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: any) {
-    return <Component {...pageProps} />;
+    return (
+        <div>
+            <Head>
+            <title>Cédric Thonus — Junior software developer</title>
+            <meta name="application-name" content="Cédric Thonus" />
+            <meta name="author" content="Cédric Thonus" />
+            <meta name="description" content="Cédric Thonus — Junior software developer" />
+            <meta name="keywords" content="développeur,developer,web developer,développeur web,bruxelles,brussels,belgique,belgium,cédric thonus,cedric thonus,thonus cedric,thonus cédric" />
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </Head>
+            <Component {...pageProps} />
+        </div>
+    );
 }
