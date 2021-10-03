3ad7c15088151a90217dc853f29bbfe2d3359e6f
Cedric Thonus <me@cedricthonus.be>
2021-10-03 19:45:26 +0200

    chore: add linkedin link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,16 +4,22 @@ export default function Contact() {
             <div className="contact-title title">Contact</div>
 
             <div className="contact-item">
-                <img src="assets/images/icons/email.svg"/>
-                cedric@thonus.dev
+                <a href="mailto:cedric@thonus.dev">
+                    <img src="assets/images/icons/email.svg"/>
+                    cedric@thonus.dev
+                </a>
             </div>
             <div className="contact-item">
-                <img src="assets/images/icons/github.svg"/>
-                <a style={{
-                  "textDecoration" : "none",
-                  "color": "white"  
-                }}
-                    href="https://github.com/cedream">https://github.com/cedream</a>
+                <a href="https://github.com/cedream">
+                    <img src="assets/images/icons/github.svg"/>
+                    GitHub
+                </a>
+            </div>
+            <div className="contact-item">
+                <a href="https://www.linkedin.com/in/c%C3%A9dric-thonus-72451a1b8/">
+                    <img src="assets/images/icons/linkedin.svg"/>
+                    Linkedin
+                </a>
             </div>
         </div>
     );
