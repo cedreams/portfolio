0e48bf0ccc378d67a8c9fb2d3d4ee6286efe1333
Cedric Thonus <me@cedricthonus.be>
2021-10-03 15:35:09 +0200

    chore: corrections

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <div className="about-content">
-                    Junior full stack <span className="title">developer</span> based in Brussels, Belgium.<br />
+                    Software <span className="title">developer</span> based in Brussels, Belgium.<br />
                     Passionate about web technologies.
         </div>
     );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,11 @@ export default function MyApp({ Component, pageProps }: any) {
     return (
         <div>
             <Head>
-            <title>Cédric Thonus — Junior software developer</title>
+            <title>Cédric Thonus — Software Developer</title>
             <meta name="application-name" content="Cédric Thonus" />
             <meta name="author" content="Cédric Thonus" />
-            <meta name="description" content="Cédric Thonus — Junior software developer" />
-            <meta name="keywords" content="développeur,developer,web developer,développeur web,bruxelles,brussels,belgique,belgium,cédric thonus,cedric thonus,thonus cedric,thonus cédric" />
+            <meta name="description" content="Cédric Thonus — Software Developer" />
+            <meta name="keywords" content="software developer,développeur,developer,web developer,développeur web,bruxelles,brussels,belgique,belgium,cédric thonus,cedric thonus,thonus cedric,thonus cédric" />
 		    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <Component {...pageProps} />
