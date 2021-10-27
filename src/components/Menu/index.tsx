75662258ad0289d803e44ed4e8a4c0616357e9fc
Cedric Thonus <me@cedricthonus.be>
2021-10-27 19:47:47 +0200

    chore: clean up

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,7 +6,7 @@ export default function Menu() {
         <div className="menu-content">
             <Logo />
             <div className="menu-links">
-                <Link smooth to="about" className="menu-link selected">About</Link>
+                <Link smooth to="about" className="menu-link">About</Link>
                 <Link smooth to="skills" className="menu-link">Skills</Link>
                 <Link smooth to="education" className="menu-link">Education</Link>
                 <Link smooth to="contact" className="menu-link contact">Contact</Link>
